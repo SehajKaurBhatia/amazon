97b1ef84cf98a28b77133e2867634a00f6746185
Sehaj <114878033+SehajKaurBhatia@users.noreply.github.com>
2024-05-22 16:04:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Electronics from './electronicsPage.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <Home/> */}
-    <Electronics/>
+    <Home/>
+{/*     <Electronics/> */}
   </React.StrictMode>,
 )
